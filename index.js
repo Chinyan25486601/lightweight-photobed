bfd5e042a8b369b551c4ce01deeb4f4bc5dce86f
秦燕 <2640855495@qq.com>
2022-02-12 23:37:02 +0800

    修改了端口

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path")
 
 const app = express();
-const PORT = 14516;
+const PORT = 14517;
 
 app.use(express.static(path.join(__dirname+"./../")));
 
